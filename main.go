fb0fbb8105188964fe0a2e706bc542189261ed98
Pyry <pyretti@outlook.com>
2025-06-07 04:47:47 +0300

    Logic & fixes for lives, victory.

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"os"
 	"slices"
+	"strings"
 	"unicode"
+	"unicode/utf8"
 
 	"github.com/pyrdelic/super-hangout/fileops"
 	"github.com/pyrdelic/super-hangout/prints"
@@ -15,6 +17,7 @@ import (
 func main() {
 	word := fileops.ReadRandomLine("wordlist.txt")
 	lives := 10
+	blanksLeft := utf8.RuneCountInString(word)
 	guessed := make([]rune, len(word))
 	//guessedLetters := ""
 
@@ -31,16 +34,20 @@ func main() {
 	}
 	defer term.Restore(int(os.Stdin.Fd()), prevState)
 
-	// TODO: inf game loop, break with ESC, game over or victory
+	// TODO: game over, victory
 
 	for {
 		// print
 		prints.Lives(lives)
 		prints.Word(word, guessed)
 		prints.Guessed(guessed)
+		fmt.Println("blanksLeft: ", blanksLeft)
 		fmt.Println()
 
+		// Read input, force to uppercase
 		char, _, err := reader.ReadRune()
+		char = unicode.ToUpper(char)
+
 		if err != nil {
 			fmt.Println("Input error:", err)
 			continue
@@ -51,19 +58,32 @@ func main() {
 			fmt.Println("Exiting.")
 			break
 		}
-		if slices.Contains(guessed, unicode.ToUpper(char)) {
+		if slices.Contains(guessed, char) {
 			fmt.Println("Already guessed")
 			continue
 		}
+		if strings.Contains(word, string(char)) {
+			blanksLeft = blanksLeft - strings.Count(word, string(char))
+		} else {
+			lives--
+		}
+
+		// game over condition
 		if lives <= 0 {
 			fmt.Println("-- GAME OVER --")
 			break
 		}
+		// victory condition
+		if blanksLeft <= 0 {
+			fmt.Println(word)
+			fmt.Println("VICTORY!")
+			break
+		}
 
-		guessed = append(guessed, unicode.ToUpper(char))
-
+		guessed = append(guessed, char)
+		// TODO: Win-condition
 		// TODO: lives logic (not in word -> lives--)
-		lives--
+		//lives--
 	}
 
 }
